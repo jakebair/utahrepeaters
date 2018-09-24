27c021d8d73aba744e5faa3d28349843728d93eb
Jacob Bair <orso.zed@gmail.com>
2018-09-23 21:31:38 -0600

    render a basic map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'utahrepeaters';
+  latitude = 40.7608;
+  longitude = -111.8910;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey:'AIzaSyA283KmhNJPT5UGp1PEljVUok2ZJ47V_D0'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
