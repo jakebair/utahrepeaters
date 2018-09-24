77b5506a5ce741eefe0482b80ffcb7ac47ea91f6
Jacob Bair <orso.zed@gmail.com>
2018-09-23 22:10:23 -0600

    current position marker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { coreDirectives } from '@agm/core/core.module';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,31 @@ export class AppComponent {
   title = 'utahrepeaters';
   latitude = 40.7608;
   longitude = -111.8910;
+  zoom: number = 8;
+
+  markers: marker[] = [];
+
+  ngOnInit() {
+    if(window.navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition(this.setPosition.bind(this));
+    }
+  }
+
+  setPosition(position) {
+    console.log(position);
+    var marker:marker = {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+      label: 'Current Location',
+      draggable: true
+    }
+    this.markers.push(marker);
+  }
+}
+
+interface marker {
+	latitude: number;
+	longitude: number;
+	label?: string;
+	draggable: boolean;
 }
