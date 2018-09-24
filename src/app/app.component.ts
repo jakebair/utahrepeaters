a75c04774d64bf1febb0ff42b8e4c1270f867769
Jacob Bair <jacob.bair@ivanti.com>
2018-09-24 14:43:05 -0600

    layout changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { LayoutService } from 'angular-admin-lte';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-    public customLayout = false;
-
-    constructor(
-        private layoutService: LayoutService
-    ) { }
-
-    ngOnInit() {
-        this.layoutService.isCustomLayout.subscribe((value: boolean) => {
-            this.customLayout = value;
-        });
-    }
+export class AppComponent {
 }
